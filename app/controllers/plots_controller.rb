00a11104ab4dd81169fd11b5df1e5bceb1ae886f
John <jdonnal@gmail.com>
2012-02-27 23:52:33 -0500

    fixed some bugs

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -9,9 +9,22 @@ class PlotsController < ApplicationController
     
   end
   def new
+    start_time=Time.parse("#{Time.now.year}-#{Time.now.month}-#{Time.now.day} #{Time.now.hour-2}:00")
+   
+    data=DataChunk.where(:start_at => start_time).first
+   
+    unless data
+      render :text=>"no data"
+      return
+    end
+    @plot_data=Hash.new()
+    @plot_data["date"]=data.start_at.in_time_zone.strftime("%D")
+    @plot_data["phase"]='1'
+    @plot_data["image_url"]=data.basename+'.prep1.png'
     
   end
   def index
+    
     
     start_time=Time.parse("#{params[:year]}-#{params[:month]}-#{params[:date]} #{params[:hour]}:00")
    
